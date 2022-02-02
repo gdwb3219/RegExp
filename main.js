ac80fc2b34bb97aed5c78cdbe3b2c04ef7dc79f4
gdwb3219 <gdwb3219@gmail.com>
2022-02-02 22:49:07 +0900

    More Pattern Exercise

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-let str = `
-010-1234-5678.
+const str = `
+010-1234-5678
 thesecon@gmail.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
-http://localhost:1234`
+` 
 
-// const regexp = new RegExp('the', 'gi')
-// const regexp = /the/gi
+
 console.log(
-  str.match(/\b\w{2,3}\b/g)
+  str.match(/(?<=@).{1,}/g)
 )
